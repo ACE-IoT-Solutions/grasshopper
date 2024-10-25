ba23150d33ec0941a4b481006c3f98757856e099
jacksonGiles <jacksongilesdev@gmail.com>
2024-10-25 17:06:09 +0000

    main init

---
+++ grasshopper-frontend/src/main.js
@@ -0,0 +1,31 @@
+// import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import router from './router'
+
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+import '@mdi/font/css/materialdesignicons.css';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: "dark",
+  },
+});
+
+const app = createApp(App);
+
+const pinia = createPinia();
+
+app
+  .use(pinia)
+  .use(router)
+  .use(vuetify)
+  .mount("#app");
