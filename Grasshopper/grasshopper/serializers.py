0a97188f0dd7b8c19538bdf0b17daafa503fb742
Douglas Plumley <douglas.plumley@dartmouth.edu>
2025-03-26 20:11:27 +0000

    Applied formatter.

--- Grasshopper/grasshopper/serializers.py
+++ Grasshopper/grasshopper/serializers.py
@@ -1,20 +1,30 @@
 """Serializers for API"""
+
+from typing import List
+
 from pydantic import BaseModel, Field
-from typing import List, Optional
+
 
 class IPAddress(BaseModel):
     """IP Address with subnet mask"""
+
     ip_address: str = Field(..., description="IP address with subnet mask")
+
 
 class IPAddressList(BaseModel):
     """List of IP addresses with subnet mask included"""
+
     ip_address_list: List[str]
+
 
 class FileList(BaseModel):
     """List of files"""
+
     file_list: List[str]
+
 
 class CompareTTLFiles(BaseModel):
     """TTL Files to compare"""
+
     ttl_1: str
     ttl_2: str
