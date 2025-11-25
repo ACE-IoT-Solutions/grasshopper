ec9f1b3481b7cf803c09a93a0602a5c9a68586c4
Jackson Giles <jackson@smallsky.dev>
2025-11-25 16:22:34 -0500

    added popup component and recurring queue api call

--- grasshopper-frontend/src/App.vue
+++ grasshopper-frontend/src/App.vue
@@ -1,14 +1,95 @@
 <template>
   <v-app id="app">
-    <RouterView :store="grasshopperStore" />
+    <v-alert
+      v-if="grasshopperStore.globalError"
+      class="error"
+      density="compact"
+      :title="grasshopperStore.alertTitle"
+      :type="grasshopperStore.alertType"
+      closable
+      @click:close="grasshopperStore.setGlobalError(null, null, null, null)"
+      >
+      <div v-html="sanitizedAlertMessage"></div>
+    </v-alert>
+    <RouterView
+      v-if="!grasshopperStore.startMenu"
+      :store="grasshopperStore"
+    />
+    <StartMenu
+      v-if="grasshopperStore.startMenu"
+      :store="grasshopperStore"
+    />
   </v-app>
 
 </template>
 
-<script setup>
+<script>
+import StartMenu from './components/StartMenu.vue'
 import { useGrasshopperStore } from "./stores/index.js";
+import DOMPurify from 'dompurify'
 
-const grasshopperStore = useGrasshopperStore();
+// const grasshopperStore = useGrasshopperStore();
+export default {
+  components: {
+    StartMenu,
+  },
+  data() {
+    return {
+      grasshopperStore: useGrasshopperStore(),
+    }
+  },
+  mounted() {
+    this.runFetchCycle();
+    this.handleRouteParams();
+  },
+  watch: {
+    '$route': {
+      // eslint-disable-next-line no-unused-vars
+      handler: function(newRoute) {
+        this.handleRouteParams();
+      },
+      immediate: true
+    }
+  },
+  computed: {
+    sanitizedAlertMessage() {
+      return DOMPurify.sanitize(
+        this.grasshopperStore.errorMessage,
+        {
+          ALLOWED_TAGS: ['b', 'i', 'em', 'strong', 'a', 'br'],
+          ALLOWED_ATTR: ['href', 'target']
+        }
+      )
+    },
+  },
+  methods: {
+    async runFetchCycle() {
+      await this.grasshopperStore.fetchQueue();
+
+      this.refreshInterval = setTimeout(() => {
+        this.runFetchCycle();
+      }, 30000);
+    },
+    handleRouteParams() {
+      
+      // Check if route params exist at all
+      if (!this.$route.params) {
+        return;
+      }
+      
+      // Check specifically for graphName
+      if (this.$route.params.graphName) {
+        this.grasshopperStore.setStartMenu(false);
+        if (this.$route.params.graphName.includes('_vs_')) {
+          this.grasshopperStore.goToGraph(true, this.$route.params.graphName);
+        } else {
+          this.grasshopperStore.goToGraph(false, this.$route.params.graphName);
+        }
+      }
+      this.grasshopperStore.setLoading(false);
+    }
+  }
+}
 </script>
 
 <style scoped>
@@ -44,6 +125,15 @@ header {
   margin: 0 1.5vw;
   border-radius: 25px;
   color: #FFFD94;
+}
+
+.error {
+  position: absolute;
+  top: 2%;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 1000;
+  max-width: 600px;
 }
 
 @media (min-width: 1024px) {
